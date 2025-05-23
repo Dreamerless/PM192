31f548fbc5a2eb66e194105fe8a55f518683c3ad
Dreamerless <122043750@upq.edu.mx>
2025-05-22 15:44:30 -0900

    A

---
+++ Practica1/Practica1.js
@@ -0,0 +1,27 @@
+let nombre = "Armando";
+const edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+
+console.log(saludo);
+
+// 
+
+const cuadrado = (numero) => {return numero * numero;};
+  
+  console.log(cuadrado(4)); // Imprime 16
+  
+//
+
+const saludoPersonalizado = (nombre, edad) => {
+    return `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+  };
+  
+
+
+  console.log(saludoPersonalizado("Isay", 37));
+  
+  sysd
